f4136ac022e03ac406bd9aa5ff6b73d31e55ef3e
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2024-11-13 14:30:04 +0530

    push

--- src/app/ads/general-psychology/[location]/page.jsx
+++ src/app/ads/general-psychology/[location]/page.jsx
@@ -540,132 +540,125 @@ export default function Component({ params }) {
                         </div>
 
                     </section>
+                       {/* services */}
+                <section className='py-8 px-4  rounded-lg'>
+                    <div className='mb-8'>
+                        <div className='mb-8 py-5'>
+                            <h1 className='text-3xl  font-semibold text-gray-800  text-center'>{allSection?.section5?.header}</h1>
+                        </div>
+                        {
+                            allSection?.section5?.services?.map((service, index) => (
+                                <div key={index} className='mb-6 '>
+                                    {/* <h1 className='text-xl font-semibold mb-5 text-center'>{service?.title}</h1> */}
+                                    <div className='grid grid-cols-2 md:grid-cols-4 items-center mb-11 gap-8 justify-center'>
+                                        {
+                                            service?.array?.map((i, index) => (
+                                                <Link href={servicesIcon[index]?.link} key={index} className='flex cursor-pointer flex-col justify-center  items-center text-center '>
+                                                    {/* <div className='w-[164px] h-[164px] rounded-full bg-[#EF6623] mb-3 '></div> */}
+                                                    <div className='w-[164px] h-[164px] flex justify-center'>
+                                                        <img src={servicesIcon[index]?.icon} className=' p-4 h-full w-full object-contain    mb-3 ' />
+                                                    </div>
+                                                    <p className='text-xl text-center font-semibold'>{i?.name}</p>
+                                                </Link>
+                                            ))
+                                        }
 
-
-
-                    {/* What We Treat */}
-                    <section className="bg-[#e9f7ff] mx-auto py-6">
-                        <h2 className="mb-12 text-center text-3xl font-bold text-teal-700">What We Treat</h2>
-                        <div className="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-4 px-2">
-                            {conditions.map((condition) => (
-                                <div key={condition.name} className="rounded-lg p-4 ">
-                                    <div>
-                                        <img
-                                            src={condition.image}
-                                            alt={condition.name}
-                                            width={100}
-                                            height={100}
-                                            className="mb-4 h-[150px] w-full rounded object-cover"
-                                        />
-                                        <p className="text-center text-teal-700">{condition.name}</p>
                                     </div>
+                                    {/* <div className='flex justify-center '>
+                                            <Link href={service?.button?.link || "/"}>
+                                                <button className='bg-[#F8A51C] hover:bg-yellow-500 active:bg-yellow-600 rounded-lg px-8 py-3 font-semibold text-sm text-white'>
+                                                    {service?.button?.text}
+                                                </button>
+                                            </Link>
+                                        </div> */}
                                 </div>
-                            ))}
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <RequestAppointment customStyle={"flex w-1/3 items-center justify-center gap-2 rounded bg-orange-500 p-3 text-white hover:bg-orange-600 focus:ring focus:ring-orange-500 mx-10"} name="Book a Consultation" />
-                        </div>
-                    </section>
+                            ))
+                        }
+                    </div>
 
-                    {/* Symptoms */}
-                    <section className="bg-gray-100 py-6">
-                        <div className="mx-auto">
-                            <h2 className="mb-4 text-center text-3xl font-bold text-teal-700">
-                                Are you experiencing any of the following symptoms?
-                            </h2>
-                            <p className="mb-8 text-center text-gray-600">
-                                If Yes, you may benefit from talking to someone
-                            </p>
-                            <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-6">
-                                {symptoms.map((symptom) => (
-                                    <div key={symptom.name} className="flex flex-col items-center rounded-lg bg-white p-6 text-center shadow">
-                                        {/* <span className="mb-2 text-3xl">{symptom.icon}</span> */}
-                                        <img
-                                            src={symptom.icon}
-                                            alt={symptom.name}
-                                            width={100}
-                                            height={100}
-                                            className="mb-4 h-[100px] w-full rounded object-cover"
-                                        />
-                                        <span className="text-sm text-gray-600">{symptom.name}</span>
-                                    </div>
-                                ))}
+                </section>
+
+
+
+                    {/* More about tms */}
+
+                <section className=' py-10 px-4 mb-6 bg-primary-div'>
+                    <div className='mb-10'>
+                        <h1 className='text-4xl text-center font-sans mb-4 font-semibold'>
+                            {allSection?.section3?.title}
+                        </h1>
+                        <p className='text-center mb-5 font-[15px] text-[#3A3A3A]'>{allSection?.section3?.para1}</p>
+                    </div>
+
+                    <div className=''>
+                        {/* hide this on medium and show alternate */}
+                        <div className='flex justify-center gap-8 mb-5 items-center py-6 rounded-lg '>
+                            <div className=' flex p-6 rounded-lg bg-primary-div '>
+                            <div className='h-[300px] w-[400px]'>
+                                <img className='w-full h-full' src={allSection?.section3?.box?.banner} />
                             </div>
-                            <div className="mt-8 text-center">
-                                <button className="rounded bg-orange-500 px-8 py-3 text-white hover:bg-orange-600">
-                                    Request an Appointment
-                                </button>
+                            <div className='flex flex-col justify-center px-8 text-gray-700'>
+                                <h3 className='font-semibold py-5 text-2xl'>NON-INVASIVE</h3>
+                                <h3 className='font-semibold py-5 text-2xl'>NO MEDICATION</h3>
+                                <h3 className='font-semibold py-5 text-2xl'>SAFE</h3>
+                                <h3 className='font-semibold py-5 text-2xl'>US FDA Approved</h3>
+                            </div>
                             </div>
                         </div>
-                    </section>
 
-
-                    <section>
-
-                        <h2 className="mb-12 text-center text-3xl font-bold text-teal-700">
-                            Our Expert Team of <span className="text-orange-500">Psychologists</span>
-                        </h2>
-                        <div className='grid grid-cols-2 justify-between bg-[#e9f7ff]'>
-                            {/* Expert Team */}
-
-                            <div className="mx-auto py-6">
-
-                                <div className="mx-auto max-w-md text-center">
-                                    <img
-                                        src="https://mindfultms1.s3.us-east-1.amazonaws.com/1730960278075-Dr.Shilpi.jpg"
-                                        alt="Ms. Shilpi Sharma"
-                                        width={200}
-                                        height={200}
-                                        className="mx-auto mb-4 rounded-full"
-                                    />
-                                    <h3 className="text-xl font-bold text-blue-600">Ms. Shilpi Sharma</h3>
-                                    <p className="font-semibold text-pink-500 text-base">SERVICES</p>
-                                    <p className="mb-2">Therapy & Assessment</p>
-                                    <p className="text-pink-500 font-semibold text-base">SPECIALIZATION</p>
-                                    <p className="mb-4 font-semibold text-sm">Depression, Anxiety, OCD, Bipolar, Marital counselling</p>
-                                    <div className='flex items-center justify-center'>
-                                        <RequestAppointment customStyle={"flex w-full items-center justify-center gap-2 rounded bg-orange-500 p-3 text-white hover:bg-orange-600 focus:ring focus:ring-orange-500 mx-10"} name="Request an Appointment" />
-                                    </div>
-                                </div>
+                        <div className='col-span-4 md:bg-primary-div'>
+                            <div className='flex flex-col justify-center mb-5 px-8  py-6 bg-[rgba(239, 102, 35, 0.3)]'>
+                                {/* <div dangerouslySetInnerHTML={{ __html: allSection?.section3?.para2 }} /> */}
+                                <p className='text-lg text-center'>At MindfulTMS, we bring 5+ years of TMS experience with 10+ clinics in India and USA.</p>
+                                <p className='text-lg text-center'><span className='font-bold'>Is TMS for me? </span>Learn how it works and if it is the right option for you.</p>
                             </div>
-
-                            {/* Testimonials Carousel */}
-
-                        </div>
-                    </section>
-
-                    {/* Why Choose Us */}
-                    <div className="bg-gray-100 grid grid-cols-3 p-10 rounded-lg ">
-                        {/* Image Section */}
-                        <div className=" mb-6 md:mb-0">
-                            <img
-                                src="/ads/why.jpg"
-                                alt="Mindful TMS"
-                                width={500}
-                                height={500}
-                                className="rounded-lg shadow-md h-[400px] object-cover"
-                            />
-                        </div>
-
-                        {/* Text Section */}
-                        <div className="col-span-2 md:pl-8">
-                            <h2 className="text-2xl md:text-3xl font-bold text-orange-500 mb-4">Why Choose Us?</h2>
-
-                            <div className="mb-6">
-                                <h3 className="text-xl font-semibold text-teal-700">Experienced Clinical Psychologists:</h3>
-                                <p className="text-gray-700 mt-2">
-                                    They are trained to assess and diagnose and provide the right therapy for you.
-                                </p>
-                            </div>
-
-                            <div>
-                                <h3 className="text-xl font-semibold text-teal-700">Confidential and Supportive Environment:</h3>
-                                <p className="text-gray-700 mt-2">
-                                    Your privacy is our priority. We provide a safe and welcoming space for you to discuss your concerns and work towards recovery.
-                                </p>
+                            <div className='flex justify-center  '>
+                                <Link href={"/pages/tms"} className='bg-[#EF6623] hover:bg-orange-500 active:bg-orange-700 rounded-lg px-8 py-3 font-semibold text-sm text-white'>
+                                    {allSection?.section3?.button?.text}
+                                </Link>
                             </div>
                         </div>
                     </div>
+                </section>
+
+                {/* wy tms */}
+                <section className='py-4 px-4 mb-11'>
+                    <div className='mb-10'>
+                        <h1 className='text-4xl text-center font-sans mb-4'>Why Choose <span className='font-semibold'> MindfulTMS?</span></h1>
+
+                        <p className='text-lg text-center'>Your well being is our mission</p>
+                    </div>
+                    <div className='flex justify-center'>
+                        <div className='grid grid-cols-2 md:grid-cols-4 gap-[70px]  items-center  justify-center'>
+                            {
+                                allSection?.section4?.services?.map((service, index) => (
+                                    <div key={index} className='flex flex-col justify-center  items-center'>
+                                        <img className='w-[132px] h-[132px] text-center' src={service?.icon} />
+                                        <p className='text-2xl font-semibold mt-3'>{service?.text}</p>
+                                    </div>
+                                ))
+                            }
+
+                        </div>
+                    </div>
+                </section>
+
+             
+
+  {/* client speaks */}
+  <section className='py-8 px-4 md:px-[100px] mt-4'>
+                    {/* <VideoComponent /> */}
+                    {/* <NewComponent videos={allSection?.section8?.videos} /> */}
+                    <h1 className='text-3xl text-gray-800 font-semibold  text-center  mb-6'>Clients Speak</h1>
+                    <TestimonialComponent />
+
+                </section>
+                     {/* our experts */}
+                <div className='bg-primary-div'>
+                    <OurDoctorSection />
+                </div>
+
+                  
                 </div>
             </>
         )
